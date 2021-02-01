56b05957af9a30c96bfc5c3c0ff1b983222491a2
NSTiwari <tiwarinitin1999@gmail.com>
2021-02-02 00:07:58 +0530

    New commit

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ async function load_model() {
     // It's possible to load the model locally or from a repo
     // You can choose whatever IP and PORT you want in the "http://127.0.0.1:8080/model.json" just set it before in your https server
     //const model = await loadGraphModel("http://127.0.0.1:8080/model.json");
-    const model = await loadGraphModel("https://raw.githubusercontent.com/NSTiwari/TFJS-Custom-Object-Detection/master/models/new-detector/model.json");
+    const model = await loadGraphModel("https://raw.githubusercontent.com/NSTiwari/TFJS-Custom-Object-Detection/master/models/new-kangaroo-detector/model.json");
     return model;
   }
 
